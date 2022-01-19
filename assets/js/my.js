08588614c5c5d32c175b5b03850526bb93b24f86
mudassir <mudassirsayyed3089@gmail.com>
2022-01-19 22:24:26 +0530

    bug fixed

--- assets/js/my.js
+++ assets/js/my.js
@@ -1,9 +1,4 @@
-const hide = document.querySelector('.active');
-const skill = document.querySelector('#skills')
-const hideMenu = () =>{
-    
-    skill.classList.remove('.active')
-}
 
 
-skill.addEventListener('click',hideMenu)
+
+
